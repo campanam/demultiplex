df943ca3a3ea4e13861d1a108b7d4a44c5a76d29
Michael G. Campana <campanam@si.edu>
2025-06-13 11:17:01 -0400

    v0.2.0

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ If no barcode file is given, the script will demultiplex by every observed barco
 The software is made available under the Smithsonian Institution [terms of use](https://www.si.edu/termsofuse).  
 
 ### Citation  
-Campana, M.G. 2025. demultiplex_by_lane v. 0.1.0. Available: https://github.com/campanam/demultiplex.  
+Campana, M.G. 2025. demultiplex_by_lane v. 0.2.0. Available: https://github.com/campanam/demultiplex.  
 
 ### Installation  
 This script requires [Ruby](www.ruby-lang.org) and the [congenlib](https://github.com/campanam/congenlib) library. Then download the `demultiplex.rb` script from this repository and place in a convenient location. You can make this program executable by running `chmod +x demultiplex.rb` and putting it in a location within your PATH.  
@@ -27,13 +27,21 @@ Options:
 `-i`, `--infile [FILE]`: Undetermined reads file to demultiplex (Required).  
 `-l`, `--lane [INTEGER]`: Specific lane number to demultiplex from an undetermined reads file (Optional, default is all lanes).  
 `-b`,`--barcodes [FILE]`: File listing samples with known barcode combinations (Optional, default is all observed index combinations).  
+`-R`, `--RC`: Try reverse complement of barcode sequences (Optional, default is only specified barcodes).  
+`-S`, `--switch`: Try reversing i5 and i7 barcode sequences (Optional, default is only specified barcodes).  
 `-H`, `--hamming [INTEGER]`: Maximum number of mismatches from known barcodes to assign read (Optional, default is 0).  
 `-u`, `--ignore`: Do not output file of unknown barcodes when using a barcode CSV (Optional, default is to output unknown barcodes).  
 `-m`, `--max [VALUE]`: Maximum number of sequences to demultiplex before quitting (Optional, default is output all reads).  
+`-T, --TOP [VALUE]`: Count the number of instances of the most frequent barcodes (Optional, default is output demultiplexed reads).  
 `-z`, `--gzip`: Gzip output sequence files (Optional, default is uncompressed).  
+`-h`, `--help`: Show help.  
+`-v`, `--version`: Show version.  
+
+### Output:  
+By default, reads matching known barcodes will be output in a FASTQ file named "<sample_name>_<barcode_combo>_<input_file_name>.fq". Unknown barcodes will be placed in an "Other_<input_file_name>.fq" file. f the `--switch` option is used, reads will be output as "<sample_name>_<barcode_combo>_<input_file_name>.fq" for non-switched indexes and "<sample_name>_S_<barcode_combo>_<input_file_name>.fq" for switched indexes.  If the `--RC` option is used, reads with known barcodes will be output as "<sample_name>_<directional_prefix>_<barcode_combo>_<input_file_name>.fq" (F for forward, R for reverse-complemented). If the `--switch` option is used with the `--RC` option, reads will be output as "<sample_name>_<directional_prefix>_<barcode_combo>_<input_file_name>.fq" for non-switched indexes and "<sample_name>_<directional_prefix>S_<barcode_combo>_<input_file_name>.fq" for switched indexes.  
 
 ### Notes:  
-1. Demultiplexing without a barcode file produces an enormous number of tiny files. I recommend running the script until the correct barcodes are made obvious by their file size and then cancelling and rerunning with a barcode CSV.
+1. Demultiplexing without a barcode file produces an enormous number of tiny files. I recommend using the --max and --top barcodes to run a subset of reads and identify the most frequent combinations.  
 2. The Ruby zlib library is not compatible with all compression settings. If the script immediately stops with a tiny result, run the following command to uncompress and recompress the Undetermined reads:
 `gunzip -c <input reads> | gzip > <rezipped reads.gz>`  
 
--- demultiplex_by_lane.rb
+++ demultiplex_by_lane.rb
@@ -1,6 +1,6 @@
 #/usr/bin/env ruby
 
-DEMBYLANEVER = '0.1.0'
+DEMBYLANEVER = '0.2.0'
 
 require 'congenlib'
 require 'optparse'
@@ -12,9 +12,12 @@ class Parser
 		args.infile = '' # Reads to demultiplex
 		args.lane = nil # Lane to demultiplex
 		args.barcodes = nil # CSV of known barcode combinations
+		args.rc = false # Try RC of index sequences
+		args.switch = false # Try switching i5 and i7
 		args.hamming = 0 # Maximum Hamming distance from known barcode
 		args.ignore = false # Don't output file of other indexes when using a list of known barcodes
 		args.max  = nil # Maximum number of sequences to demultiplex
+		args.top = nil # Return most frequent barcodes
 		args.gzip = false # Gzip output
 		opt_parser = OptionParser.new do | opts |
 			opts.banner = "Demultiplex by Lane Version " + DEMBYLANEVER
@@ -31,6 +34,12 @@ class Parser
 			opts.on("-b","--barcodes [FILE]", String, "File listing samples with known barcode combinations") do |barcodes|
 				args.barcodes = barcodes
 			end
+			opts.on("-R", "--RC", "Try reverse complement of barcode sequences") do |rc|
+				args.rc = true
+			end
+			opts.on("-S", "--switch", "Try reversing i5 and i7 barcode sequences") do |rc|
+				args.switch = true
+			end
 			opts.on("-H", "--hamming [VALUE]", Integer, "Maximum number of mismatches from known barcodes to assign read") do |hamming|
 				args.hamming = hamming.to_i
 			end
@@ -40,12 +49,14 @@ class Parser
 			opts.on("-m", "--max [VALUE]", Integer, "Maximum number of sequences to demultiplex before quitting") do |max|
 				args.max = max
 			end
+			opts.on("-T", "--TOP [VALUE]", Integer, "Count the number of instances of the most frequent barcodes") do |top|
+				args.top = top
+			end
 			opts.on("-z", "--gzip", "Gzip output sequence files") do
 				args.gzip = true
 			end
 			# Ideas for additional options:
 			# --top X: count the number of instances of barcode and return top X barcodes
-			# --tryRC: try the reverse complement of the barcode sequence as this is often a source of error
 			opts.on_tail("-h", "--help", "Show help") do
 				puts opts
 				exit
@@ -72,20 +83,95 @@ def write_output # Method to write output in the filehash
 		end
 	end
 end
+#----------------------------------------------------------------------------------------- 
+def output_barcodes # Method to output top X barcodes
+	puts "Barcode\tCount"
+	print $filehash.sort_by { |key,value| value}
+	for key in $filehash.keys
+		puts key + "\t" + $filehash[key].to_s
+	end
+end
 #-----------------------------------------------------------------------------------------
 ARGV[0] ||= '-h'
 $options = Parser.parse(ARGV)
 $stem = '_' + $options.infile.gsub('.gz','') # Output file stem modifier
 $samplehash = {} #Hash associating barcode with sample ID. Ignored if no barcode file.
 $filehash = {} #Hash associating barcode with output files
+
+build_rc_hash if $options.rc
 	
 unless $options.barcodes.nil? # Read in known barcodes
 #Barcode file format is headerless CSV with two columns: sample,index1+index2
 	gz_file_open($options.barcodes) do |f2|
 		while line = f2.gets
 			line_arr = line.strip.split(',')
-			$samplehash[line_arr[1]] = line_arr[0] + "_"
-			$filehash[line_arr[1]] = '' # Set up output
+			if $options.rc
+				index_arr = line_arr[1].split('+')
+				if index_arr.size == 1 # Single-indexed run
+					$samplehash[line_arr[1]] = line_arr[0] + "_F_"
+					$samplehash[rc(line_arr[1])] = line_arr[0] + "_R_"
+					$options.top.nil? ? $filehash[line_arr[1]] = '' : $filehash[line_arr[1]] = 0 # Set up output
+					$options.top.nil? ? $filehash[rc(line_arr[1])] = '' :$filehash[rc(line_arr[1])] = 0 # Set up output
+					if $options.switch
+						STDERR.puts "Barcodes are single-indexed. Turning off switch option."
+						STDERR.puts "Error found here in barcode CSV: " + line
+						$options.switch = false
+					end
+				else
+					# Unmodified indexes
+					$samplehash[line_arr[1]] = line_arr[0] + "_FF_"
+					$options.top.nil? ? $filehash[line_arr[1]] = '' : $filehash[line_arr[1]] = 0 # Set up output
+					index1 = index_arr[0]
+					index2 = index_arr[1]
+					rc_index1 = rc(index1)
+					rc_index2 = rc(index2)
+					# RC both indexes
+					both_rc = rc_index1 + "+" + rc_index2
+					$samplehash[both_rc] = line_arr[0] + "_RR_"
+					$options.top.nil? ? $filehash[both_rc] = '' : $filehash[both_rc] = 0 # Set up output
+					# RC index1
+					for_rc = rc_index1 + "+" + index2
+					$samplehash[for_rc] = line_arr[0] + "_RF_"
+					$options.top.nil? ? $filehash[for_rc] = '' : $filehash[for_rc] = 0 # Set up output
+					# RC index2
+					rev_rc = index1 + "+" + rc_index1
+					$samplehash[rev_rc] = line_arr[0] + "_FR_"
+					$options.top.nil? ? $filehash[rev_rc] = '' : $filehash[rev_rc] = 0 # Set up output
+					if $options.switch
+						# Switch original indexes
+						switch = index_arr[1] + '+' + index_arr[0]
+						$samplehash[switch] = line_arr[0] + "_FFS_"
+						$options.top.nil? ? $filehash[switch] = '': $filehash[switch] = 0
+						#RC both and switch
+						both_rcS = rc_index2 + "+" + rc_index1
+						$samplehash[both_rcS] = line_arr[0] + "_RRS_"
+						$options.top.nil? ? $filehash[both_rcS] = '' : $filehash[both_rcS] = 0 # Set up output
+						# RC index1 and switch
+						for_rcS = rc_index2 + "+" + index1
+						$samplehash[for_rcS] = line_arr[0] + "_RFS_"
+						$options.top.nil? ? $filehash[for_rcS] = '' : $filehash[for_rcS] = 0 # Set up output
+						# RC index2 and switch
+						rev_rcS = index2 + "+" + rc_index1
+						$samplehash[rev_rcS] = line_arr[0] + "_FRS_"
+						$options.top.nil? ? $filehash[rev_rcS] = '' : $filehash[rev_rcS] = 0 # Set up output
+					end
+				end
+			else				
+				$samplehash[line_arr[1]] = line_arr[0] + "_"
+				$options.top.nil? ? $filehash[line_arr[1]] = '' : $filehash[line_arr[1]] = 0 # Set up output
+				if $options.switch
+					index_arr = line_arr[1].split('+')
+					if index_arr.size == 1
+						STDERR.puts "Barcodes are single-indexed. Turning off switch option."
+						STDERR.puts "Error found here in barcode CSV: " + line
+						$options.switch = false
+					else
+						switch = index_arr[1] + '+' + index_arr[0]
+						$samplehash[switch] = line_arr[0] + "_S_"
+						$options.top.nil? ? $filehash[switch] = '' : $filehash[switch] = 0
+					end
+				end	
+			end
 		end
 	end
 	$filehash['Other'] = '' # Entry for barcodes that don't match known indexes
@@ -110,9 +196,9 @@ gz_file_open($options.infile) do |f1| # Read and demultiplex reads
 			barcode = line_arr[-1]
 			@seqcount += 1
 			if $filehash.keys.include?(barcode) # If a barcode/sample file given, all these names already exist in the hash
-				$filehash[barcode] << line
+				$options.top.nil? ? $filehash[barcode] << line : $filehash[barcode] += 1
 			elsif $options.barcodes.nil? # Only add this barcode if extracting EVERY barcode
-				$filehash[barcode] = line
+				$options.top.nil? ? $filehash[barcode] = line : $filehash[barcode] = 1
 			else
 				if $options.hamming > 0
 					for key in $filehash.keys
@@ -122,16 +208,27 @@ gz_file_open($options.infile) do |f1| # Read and demultiplex reads
 						end
 					end
 					if $filehash.keys.include?(barcode)
-						$filehash[barcode] << line
+						$options.top.nil? ? $filehash[barcode] << line : $filehash[barcode] += 1
 					else
 						barcode = 'Other'
-						$filehash[barcode] << line unless $options.ignore # Don't append lines that will never be written or deleted
+						if $options.top.nil?
+							$filehash[barcode] << line unless $options.ignore # Don't append lines that will never be written or deleted
+						else
+							$filehash[barcode] += 1
+						end
 					end
 				else
 					barcode = 'Other' # Add unknown barcodes to other file if using barcode/sample list
-					$filehash[barcode] << line unless $options.ignore # Don't append lines that will never be written or deleted
+					if $options.top.nil?
+						$filehash[barcode] << line unless $options.ignore # Don't append lines that will never be written or deleted
+					else
+						$filehash[barcode] += 1
+					end
 				end
 			end
+		elsif !$options.top.nil?
+			@counter += 1
+			next
 		else
 			if $options.lane.nil?
 				$filehash[barcode] << line
@@ -140,10 +237,14 @@ gz_file_open($options.infile) do |f1| # Read and demultiplex reads
 			end
 		end
 		@counter += 1
-		write_output if @counter % 1000000 == 0 # Write output every million lines
+		write_output if @counter % 1000000 == 0 # Write output every million lines.
 	end
 end
-write_output # write final output
+if !$options.top.nil?
+	output_barcodes # Output most frequent barcodes
+else
+	write_output # write final output
+end
 if $options.gzip
 	for key in $filehash.keys
 		next if (key == 'Other' && $options.ignore)
